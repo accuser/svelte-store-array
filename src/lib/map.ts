fc3f3fb7929f2803b7eeef3c8dfdaebbe7a0950b
Matthew Gibbons <mhgibbons@me.com>
2022-09-28 08:48:30 +0100

    Typing

--- src/lib/map.ts
+++ src/lib/map.ts
@@ -1,9 +1,7 @@
 import { derived, type Readable } from 'svelte/store';
 
-const map = <T, U>(
-	store: Readable<T[]>,
-	callbackFn: (value: T, index: number, array: T[]) => U
-): Readable<U[]> => derived(store, (values) => values.map(callbackFn));
+const map = <T, U>(store: Readable<T[]>, callback: MapFn<T, U>): Readable<U[]> =>
+	derived(store, (values) => values.map(callback));
 
 export default map;
 export { map };
