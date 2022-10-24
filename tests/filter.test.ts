18287fb58921c2192bc2a2ceebb16b24ae3e5563
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-10-24 07:56:56 +0100

    Added tests for `compact`

--- tests/filter.test.ts
+++ tests/filter.test.ts
@@ -1,4 +1,4 @@
-import { filter } from '$lib';
+import { compact, filter } from '$lib';
 import { readable, get, type Readable } from 'svelte/store';
 import { beforeEach, describe, expect, it } from 'vitest';
 
@@ -41,6 +41,44 @@ describe('filter', () => {
 			const value = get(filter(numbers, (value) => value % 2 !== 0));
 
 			expect(value.length).toBe(5);
+		});
+	});
+});
+
+describe('compact', () => {
+	describe('with an array', () => {
+		beforeEach(async (context) => {
+			context.numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+		});
+
+		it('returns a store', ({ numbers }) => {
+			const store = filter(numbers, () => true);
+
+			expect(store.subscribe).toBeDefined();
+		});
+
+		it('remove falsy values', ({ numbers }) => {
+			const value = get(compact(numbers));
+
+			expect(value.length).toBe(9);
+		});
+	});
+
+	describe('with a store', () => {
+		beforeEach(async (context) => {
+			context.numbers = readable([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+		});
+
+		it('returns a store', ({ numbers }) => {
+			const store = filter(numbers, () => true);
+
+			expect(store.subscribe).toBeDefined();
+		});
+
+		it('filters the values', ({ numbers }) => {
+			const value = get(compact(numbers));
+
+			expect(value.length).toBe(9);
 		});
 	});
 });
